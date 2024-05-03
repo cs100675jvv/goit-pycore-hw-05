c44583d049ce3154bd316bd308c4abcb9bafee72
cs100675jvv <cs100675jvv@gmail.com>
2024-05-03 14:41:02 +0300

    task-1

---
+++ task-1/main.py
@@ -0,0 +1,14 @@
+from cache_fibonacci import caching_fibonacci
+
+def main():
+
+    fib = caching_fibonacci()
+    print(fib(10))
+    print(fib(15)) 
+
+
+
+
+
+if __name__ == "__main__":
+    main()
